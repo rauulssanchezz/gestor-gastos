5e674482294514a3bba242ccfe8dfc7b11020bde
rauulssanchezz <rauulssanchezz@gmail.com>
2025-12-09 16:34:38 +0100

    blacklists de token para el logout

--- src/main/java/com/example/utils/JwtUtil.java
+++ src/main/java/com/example/utils/JwtUtil.java
@@ -1,30 +1,39 @@
 package com.example.utils;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class JwtUtil {
 
     @Value("${jwt.secret}")
-    private String jwtSecret;
+    private String JWT_SECRET;
 
     @Value("${jwt.expiration}")
-    private Date expiration;
+    private Date EXPIRATION;
 
     @Value("${jwt.signature.algorithm}")
     private SignatureAlgorithm signatureAlgorithm;
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public JwtUtil(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
     
     private Key getSigningKey() {
-        byte[] keyBytes = Base64.getDecoder().decode(this.jwtSecret);
+        byte[] keyBytes = Base64.getDecoder().decode(this.JWT_SECRET);
 
         return Keys.hmacShaKeyFor(keyBytes);
     }
@@ -36,8 +45,20 @@ public class JwtUtil {
                 .claims(extraClaims) 
                 .subject(id) 
                 .issuedAt(issuedAt) 
-                .expiration(this.expiration) 
+                .expiration(this.EXPIRATION) 
                 .signWith(getSigningKey(), this.signatureAlgorithm) 
                 .compact(); 
+    }
+
+    public void blacklistToken(String jti, long ttlSegundos) {
+        this.redisTemplate.opsForValue().set(jti, "revoked", ttlSegundos, TimeUnit.SECONDS);
+    }
+
+    public boolean isBlacklisted(String jti) {
+        return this.redisTemplate.hasKey(jti);
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parser().verifyWith(Keys.hmacShaKeyFor(Base64.getDecoder().decode(JWT_SECRET))).build().parseSignedClaims(token).getPayload();
     }
 }
